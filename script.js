bae598a005b402f15035f4b1339d03e0e5d54736
raoufzouaoui <raoufzouaouo06@gmail.com>
2023-06-07 17:43:51 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for( tablink of tablinks){
+        console.log(tablink);
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab")
+}
+
+
+/ SideMenu /
+
+var sidemenu = document.getElementById('sidemenu');
+
+function openmenu(){
+    sidemenu.style.right="0";
+}
+
+function closemenu(){
+    sidemenu.style.right="-200px";
+}
+
+/ contact form /
+
+ 
+
